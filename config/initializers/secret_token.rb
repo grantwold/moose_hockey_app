995ee597d9119f177c16a656aa3d62b2db83bc25
Grant Wold <grantwold@gmail.com>
2013-08-28 14:09:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MooseHockeyApp::Application.config.secret_token = '61841f42cd9d500f6c8cb58fe3b217e68e652661da1c75d4fe89e6295eca6fb38138cfc3608004716b1541f0ee48b57419368d543b643ae9c00af9512dff54f2'
